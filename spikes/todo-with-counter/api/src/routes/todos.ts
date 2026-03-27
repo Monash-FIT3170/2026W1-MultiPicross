7c5db95f3b10429cc1dd6a172ee9fc4d6ad5209a
Christian <christian.vourgoutzis@hotmail.com>
2026-03-27 14:57:25 +1100

    Implemented todos.ts post, delete, patch

--- spikes/todo-with-counter/api/src/routes/todos.ts
+++ spikes/todo-with-counter/api/src/routes/todos.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { eq } from "drizzle-orm"
 import { db } from "../db/index.js";
 import { todos as todosTable } from "../db/schema.js";
 
@@ -11,11 +12,24 @@ todos.get("/", async (c) => {
 
 todos.post("/", async (c) => {
   // TODO: parse and validate request body
-  return c.json({ message: "created" }, 201);
+  const { title } = await c.req.json();
+  const newTodo = await db.insert(todosTable).values({ title }).returning()
+  return c.json(newTodo[0], 201);
 });
 
 // TODO: implement DELETE /todos/:id
+todos.delete("/:id", async (c) => {
+  const id = Number(c.req.param("id"));
+  await db.delete(todosTable).where(eq(todosTable.id, id));
+  return c.json({ message: "deleted" });
+})
 
 // TODO: implement PATCH /todos/:id (e.g. to mark as completed)
+todos.patch("/:id", async (c) => {
+  const id = Number(c.req.param("id"));
+  const { completed } = await c.req.json();
+  const updated = await db.update(todosTable).set({ completed }).where(eq(todosTable.id, id)).returning();
+  return c.json(updated[0]);
+})
 
 export default todos;
