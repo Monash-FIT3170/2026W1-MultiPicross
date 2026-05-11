86361c3dd364308114e420601fc7b3a417ec88d1
De-arne Baker <dbak0009@student.monash.edu>
2026-05-11 15:59:07 +1000

    integrated main menu and singleplayer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,24 @@
 import { useState } from "react";
 import MainMenu from "./pages/MainMenu";
 import heroImg from "./assets/hero.png";
-import "./App.css";
 import Board from "./components/Board";
+import { Singleplayer } from "./pages/Singleplayer";
+
 
 function App() {
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-        </div>
-        <div>
-          <h1>MultiPicross</h1>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <h2>Board desc</h2>
-          <p>Left click to fill/unfill. Right click to place/remove a cross.</p>
-        </div>
-        <div id="social">
-          <Board />
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  );
   const [page, setPage] = useState("menu");
-
+  
   function renderPage() {
     switch (page) {
       case "menu":
       default:
         return <MainMenu navigate={setPage} />;
+      case "singleplayer":
+        return <Singleplayer navigate={setPage} />;
+      }
     }
-  }
 
-  return <>{renderPage()}</>;
+    return <>{renderPage()}</>;
 }
 
 export default App;
--- frontend/src/pages/Singleplayer.tsx
+++ frontend/src/pages/Singleplayer.tsx
@@ -1,12 +1,28 @@
-import { Button } from '../components/Button';
+import Board from '../components/Board';
 import './PagePlaceholder.css';
+import React from 'react';
 
 export function Singleplayer() {
   return (
     <div className="page-placeholder">
       <h1>Singleplayer</h1>
       <p>Puzzle selection will go here.</p>
-      <Button to="/" variant="secondary">Back to Menu</Button>
+      <button to="/" variant="secondary">Back to Menu</button>
+
+       <div className="ticks"></div>
+      
+            <section id="next-steps">
+              <div id="docs">
+                <h2>Board desc</h2>
+                <p>Left click to fill/unfill. Right click to place/remove a cross.</p>
+              </div>
+              <div id="social">
+                <Board />
+              </div>
+            </section>
+      
+            <div className="ticks"></div>
+            <section id="spacer"></section>
     </div>
   );
 }
